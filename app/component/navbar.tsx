ad3271aea0eeff6569ca724cb425972c9202f3be
Rawit101 <sorawitardpru@gmail.com>
2025-09-12 22:04:51 +0700

    responsive_navbar

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -1,16 +1,52 @@
+'use client'
+
 import React from 'react'
+import { useState } from 'react'
 
 function Navbar() {
-    return (
-        <nav>
-            <ul className='flex  bg-slate-300 p-4 text-black justify-center gap-10'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Blog</li>
-            </ul>
-        </nav>
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+    return (
+        <nav className='bg-gray-400 p-4'>
+            <div className='flex justify-between items-center'>
+                <div className='text-white font-bold'>My App</div>
+                {/* Toggle button for mobile menu */}
+                <div className='md:hidden'>
+                    <button id="menu-toggle" className='text-white' onClick={toggleMenu}><svg
+                        fill='none'
+                        stroke='currentColor'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth="2"
+                        viewBox='0 0 24 24'
+                        className='w-6 h-6'
+                    >
+                        <path d='M4 6h16M4 12h16M4 18h16' /></svg>
+                    </button>
+                </div>
+                <ul className='hidden md:flex space-x-4'>
+                    <li><a href="#" className='text-white'>Home</a></li>
+                    <li><a href="#" className='text-white'>About</a></li>
+                    <li><a href="#" className='text-white'>Contact</a></li>
+                    <li><a href="#" className='text-white'>Blog</a></li>
+                </ul>
+            </div>
+
+            {/* Mobile menu */}
+
+            {isMenuOpen ? (
+                <ul className='flex-col md:hidden space-x-4'>
+                    <li><a href="#" className='text-white'>Home</a></li>
+                    <li><a href="#" className='text-white'>About</a></li>
+                    <li><a href="#" className='text-white'>Contact</a></li>
+                    <li><a href="#" className='text-white'>Blog</a></li>
+                </ul>
+            ) : null}
+        </nav>
     )
 }
 
