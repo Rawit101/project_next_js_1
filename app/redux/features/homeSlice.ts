9c0a105d4250eb7221f3ceb26b7d917fb91e4618
Rawit101 <sorawitardpru@gmail.com>
2025-09-24 22:30:24 +0700

    features_homeslice

---
+++ app/redux/features/homeSlice.ts
@@ -0,0 +1,30 @@
+import { HomeSlice } from "@/app/models/HomeSlice"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { stat } from "fs"
+
+const initialState: HomeSlice = {
+    isBannerVisible: localStorage.getItem("isBannerVisibleClosed") === "true" ? false : true,
+    isDarkMode: false,
+    isLoading: false,
+}
+
+export const homeSlice = createSlice({
+    name: 'homeSlice',
+    initialState,
+    reducers: {
+        updateBanner: (state, action: PayloadAction<boolean>) => {
+            return { ...state, isBannerVisible: action.payload }
+        },
+
+        updateDarkMode: (state, action: PayloadAction<boolean>) => {
+            return { ...state, isDarkMode: action.payload }
+        },
+
+        updateLoading: (state, action: PayloadAction<boolean>) => {
+            return { ...state, isLoading: action.payload }
+        }
+    }
+})
+
+export const { updateBanner, updateDarkMode, updateLoading } = homeSlice.actions
+export default homeSlice.reducer
